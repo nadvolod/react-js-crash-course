a631224a7e299a66cb6f9ca1300e127501a75336
nadvolod <nadvolod@gmail.com>
2021-03-06 15:51:15 -0500

    Add default props to Header

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -1,5 +1,6 @@
 import Header from './components/Header'
 
+// This is a function component that will render
 function App() {
   return (
     <div className="App">
@@ -8,4 +9,16 @@ function App() {
   );
 }
 
-export default App;
+// We can also create a class based component like this
+// use shortcut `rcep`
+// import React from 'react'
+
+// export class App extends React.Component {
+//   render() {
+//     return (
+//       <h1>Hello from a class</h1>
+//     )
+//   }
+// }
+
+export default App
--- react-task-tracker/src/components/Header.js
+++ react-task-tracker/src/components/Header.js
@@ -1,10 +1,18 @@
 // With the extension installed, running 'rafce' will create a new component
-const Header = () => {
+const Header = (props) => {
     return (
         <header>
-            <h1>Task Tracker</h1>
+            <h1>{props.title}</h1>
         </header>
     )
+}
+
+// We can set default props on our component that will appear unless
+// they are overriden by a call from the component like this:
+// <Header title='Hello'/>
+
+Header.defaultProps = {
+    title: 'Task Tracker'
 }
 
 export default Header
