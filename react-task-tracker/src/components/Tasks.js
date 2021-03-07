ebc6a107940ed0ff9efae29583d8f220db1039e0
nadvolod <nadvolod@gmail.com>
2021-03-07 15:15:32 -0500

    move tasks array into Tasks component

--- react-task-tracker/src/components/Tasks.js
+++ react-task-tracker/src/components/Tasks.js
@@ -1,26 +1,29 @@
-import React from 'react'
-
-const tasks = [
-    {
-        id: 1,
-        text: 'food',
-        day: 'x',
-        reminder: false
-    },
-    {    
-        id: 2,
-        text: 'hello',
-        day: 'x',
-        reminder: false
-    }
-]
-
+import {useState} from 'react'
 
 const Tasks = () => {
+    // Everything is still the same, however, now the list is part
+    // of our component state
+    const [tasks, setTasks] = useState(
+        [
+            {
+                id: 1,
+                text: 'food',
+                day: 'x',
+                reminder: false
+            },
+            {    
+                id: 2,
+                text: 'hello',
+                day: 'x',
+                reminder: false
+            }
+        ]
+    )
     return (
         <>
             {tasks.map((task) => (
-                <h3>{task.text}</h3>
+                // A child in a list should have a unique key
+                <h3 key={task.id}>{task.text}</h3>
             ))}
         </>
     )
