9943398073e58fe83d3674da7152ba3dcc579b7b
nadvolod <nadvolod@gmail.com>
2021-03-07 11:18:31 -0500

    Create an onClick()

--- react-task-tracker/src/components/Button.js
+++ react-task-tracker/src/components/Button.js
@@ -1,11 +1,24 @@
-import React from 'react'
+// impt
+import PropTypes from 'prop-types'
 
-const Button = ({color, text}) => {
+const Button = ({color, text, onClick}) => {
     return (
-        <button style={{backgroundColor: color}} 
+        <button
+        onClick={onClick} 
+        style={{backgroundColor: color}} 
         className='btn'>{text}
         </button>
     )
 }
 
+//These are default values
+Button.defaultProps = {
+    color: 'steelblue'
+}
+
+// These are types for the properties such as having `text` be a string
+Button.propTypes ={
+    text: PropTypes.string,
+    color: PropTypes.string
+}
 export default Button
--- react-task-tracker/src/components/Header.js
+++ react-task-tracker/src/components/Header.js
@@ -2,14 +2,18 @@ import Button from './Button'
 
 // With the extension installed, running 'rafce' will create a new component
 const Header = ({title}) => {
+    //Defining a click function that will be passed to the onClick property in the Button
+    const onClick = () =>{
+        console.log('click')
+    }
+
     return (
         // The header is pulling styling from 'header' in index.css
         <header className='header'>
             <h1>{title}</h1>
             {/* The button is pulling styling from our index.css
             by referencing class 'btn' */}
-            <Button color='green' text='Hello'/>
-            <Button color='blue' text='Hello 2'/>
+            <Button color='green' text='Add' onClick={onClick}/>
         </header>
     )
 }
