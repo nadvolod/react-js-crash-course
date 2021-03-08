c1b403285f0868be0b78be1d4ab7733a94f17a6b
nadvolod <nadvolod@gmail.com>
2021-03-08 09:12:00 -0500

    Add icons package and icons

--- react-task-tracker/src/components/Task.js
+++ react-task-tracker/src/components/Task.js
@@ -1,10 +1,13 @@
-import React from 'react'
+// We are utilizing npm package react-icons to add icons to our markup
+import {FaTimes} from 'react-icons/fa'
 
 // rafce - React arrow function component export
 const Task = ({task}) => {
     return (
         <div className='task'>
-            <h3>{task.text}</h3>
+            {/* FaTimes is a component from react-icons that creates an X */}
+            <h3>{task.text} <FaTimes />
+            </h3>
             <p>{task.day}</p>
         </div>
     )
