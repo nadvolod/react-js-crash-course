249557b00498ac230cd6a7223c10b9faafed3cb1
nadvolod <nadvolod@gmail.com>
2021-03-09 09:12:18 -0500

    add No Tasks message

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -1,37 +1,37 @@
 import Header from './components/Header'
 import Tasks from './components/Tasks'
-import {useState} from 'react'
+import { useState } from 'react'
 
 // This is a function component that will render
 function App() {
   const [tasks, setTasks] = useState(
     [
-        {
-            id: 1,
-            text: 'food',
-            day: 'x',
-            reminder: false
-        },
-        {    
-            id: 2,
-            text: 'hello',
-            day: 'x',
-            reminder: false
-        }
+      {
+        id: 1,
+        text: 'food',
+        day: 'x',
+        reminder: false
+      },
+      {
+        id: 2,
+        text: 'hello',
+        day: 'x',
+        reminder: false
+      }
     ]
-)
+  )
 
-// Delete a task
-// 'nfn' to create a const function with param
-const deleteTask = (id) => {  
-  console.log('delete', id)
-}
-
+  // Delete a task
+  // 'nfn' to create a const function with param
+  const deleteTask = (id) => {
+    // When we call deleteTask(), React will show the tasks that are not filtered out by id
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
 
   return (
     <div className="App">
       <Header />
-      <Tasks tasks={tasks} onDelete={deleteTask}/>
+      {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask} />) : ('No tasks')}
     </div>
   );
 }
