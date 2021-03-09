d1b951db4220ce3373f88cea65c22ada39e797d6
nadvolod <nadvolod@gmail.com>
2021-03-09 17:54:31 -0500

    Add reminder task notification bar

--- react-task-tracker/src/components/Task.js
+++ react-task-tracker/src/components/Task.js
@@ -3,9 +3,13 @@ import {FaTimes} from 'react-icons/fa'
 
 // rafce - React arrow function component export
 const Task = ({task, onDelete, onToggle}) => {
+    const isReminderEnabled = `${task.reminder ? 'reminder': ''}`
+    const reminderClassName = `task ${isReminderEnabled}`
+
     return (
         // Now double clicking on the task div will perform the onToggle() which is passed down from App.js
-        <div className='task' onDoubleClick={()=> onToggle(task.id)}>
+        // Also, we render a different className depending on whether the 'reminder' is true/false
+        <div className={reminderClassName} onDoubleClick={()=> onToggle(task.id)}>
             {/* FaTimes is a component from react-icons that creates an X */}
             <h3>{task.text} 
             <FaTimes style={{ color: 'red', cursor: 'pointer'}} 
