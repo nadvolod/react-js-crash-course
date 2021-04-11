94bfc435eb89c357c950bf35f02ed31475f069fd
nadvolod <nadvolod@gmail.com>
2021-04-11 12:59:28 -0400

    add code coverage capabilities

---
+++ react-task-tracker/jest.config.js
@@ -0,0 +1,15 @@
+const path = require('path')
+
+module.exports = {
+  testEnvironment: 'jest-environment-jsdom',
+  moduleDirectories: [
+    'node_modules',
+    path.join(__dirname, 'src'),
+    'shared',
+    path.join(__dirname, 'test'),
+  ],
+  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
+  snapshotSerializers: ['jest-emotion'],
+  // collect coverage from all the files under src that end in .js
+  collectCoverageFrom: ['**/src/**/*.js'],
+}
