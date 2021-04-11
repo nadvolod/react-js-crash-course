120775e6b7ceddb6f4a5753fe518061651b8f090
nadvolod <nadvolod@gmail.com>
2021-04-11 16:25:16 -0400

    Changed Button to Button.jsx

---
+++ react-task-tracker/src/components/Button.jsx
@@ -0,0 +1,25 @@
+// impt
+import PropTypes from 'prop-types'
+
+const Button = ({color, text, onClick}) => {
+    return (
+        <button
+        onClick={onClick} 
+        style={{backgroundColor: color}} 
+        className='btn'>{text}
+        </button>
+    )
+}
+
+//These are default values
+Button.defaultProps = {
+    color: 'steelblue'
+}
+
+// These are types for the properties such as having `text` be a string
+Button.propTypes ={
+    text: PropTypes.string,
+    color: PropTypes.string,
+    onClick: PropTypes.func
+}
+export default Button
